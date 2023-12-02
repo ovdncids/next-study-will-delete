759aaabee665c8f1184ce46c25001c9f6d96e28a
ovdncids <ovdncids@nate.com>
2023-12-02 12:01:54 +0900

    env-cmd

--- services/usersServices.ts
+++ services/usersServices.ts
@@ -3,27 +3,27 @@ import {User} from "@/types/usersTypes"
 export const usersServices = {
   usersRead: async () => {
     try {
-      const res = await fetch("http://localhost:3000/api/users", {cache: "no-store"})
+      const res = await fetch(`${process.env.NEXT_PRIVATE_BACKEND_API_URL}/api/users`, {cache: "no-store"})
       return res.json()
     } catch (error) {
       return []
     }
   },
   usersCreate: async (user: User) => {
-    const res = await fetch("http://localhost:3000/api/users", {
+    const res = await fetch("/proxy/api/users", {
       method: "POST",
       body: JSON.stringify(user)
     })
     return res.json()
   },
   usersDelete: async (index: number) => {
-    const res = await fetch("http://localhost:3000/api/users/" + index, {
+    const res = await fetch(`/proxy/api/users/${index}`, {
       method: "DELETE"
     })
     return res.json()
   },
   usersUpdate: async (index: number, user: User) => {
-    const res = await fetch("http://localhost:3000/api/users/" + index, {
+    const res = await fetch(`/proxy/api/users/${index}`, {
       method: "PATCH",
       body: JSON.stringify(user)
     })
